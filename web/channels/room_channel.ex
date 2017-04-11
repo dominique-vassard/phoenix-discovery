8c6698d8a917955ce4f64d26c7e78d6c299b7589
Dominique VASSARD <dominique.vassard@gmail.com>
2017-04-11 22:46:07 +0200

    Chapter: Test channel

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -14,8 +14,25 @@ defmodule HelloPhoenix.RoomChannel do
     {:noreply, socket}
   end
 
+  def handle_in("ping", payload, socket) do
+    {:reply, {:ok, payload}, socket}
+  end
+
+  def handle_in("shout", payload, socket) do
+    broadcast! socket, "shout", payload
+    {:noreply, socket}
+  end
+
+  intercept ["new_msg"]
+
   def handle_out("new_msg", payload, socket) do
     push socket, "new_msg", payload
     {:noreply, socket}
   end
+
+  def handle_out(event, payload, socket) do
+    push socket, event, payload
+    {:noreply, socket}
+  end
+
 end
